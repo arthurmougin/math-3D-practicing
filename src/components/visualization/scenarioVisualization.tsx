2e19a4be3389584c6568be7bf2b74f5d4f63ae3d
arthurM <arthur.mougin@live.fr>
2025-11-22 17:42:32 +0100

    note result after exploration of Math value usage in threejs math lib

--- src/components/visualization/scenarioVisualization.tsx
+++ src/components/visualization/scenarioVisualization.tsx
@@ -2,6 +2,22 @@ import { Suspense } from "react";
 import { useScenarioStore } from "../../stores/scenarioStore";
 import { Result } from "./result";
 import { Parameter } from "./parameter";
+/**
+ * TODO List of all types of representation to implement
+ * - Position (free vertex for vector2, vector3)
+ *     - on 1 or more axes
+ *     - possibly with arrows (for offsets)
+ *     - possibly normalized (for direction vectors )
+ * - Rotation (axes with rotation ?) 
+ *     - on 1 or more axes
+ * - Scale
+ *     - on 1 or more axes
+ * - Position + rotation (free rotating vertex for normals)
+ * - Full transform (matrix) box
+ * - Projection ?
+ * - Non geometric values ?
+ *    - float, int, interpolation factors (0-1) ?
+ */
 
 interface ScenarioVisualizationProps {
   showresult?: boolean; // Whether to show the result
